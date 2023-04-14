9397bbc7ebfe5a9fd73f57cb7de7bd03a72e5a6a
Alex Stark <math.lau2007@gmail.com>
2023-04-14 09:22:19 +0200

    added cookie popup
    cookies

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const popup = document.getElementById("cookieConsent");
+var feur = Math.floor(Math.random()*10);
+if (feur!=1){
+  popup.className='feur';
+}
+else {
+  const acceptBtn = document.getElementById("acceptButton");
+  const declineBtn = document.getElementById("declineButton");
+
+  acceptBtn.addEventListener("click", function() {
+    const fileUrl = "cookies.jpeg";
+    const fileName = "cookies.jpeg";
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = fileName;
+    a.click();
+  });
+
+  declineBtn.addEventListener("click", function() {
+    popup.className='feur';
+  });
+}
+
+
+
+
 window.onscroll = function() {setShadowBox()};
 
 function setShadowBox() {
